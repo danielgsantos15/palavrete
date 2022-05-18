30967243e11cf6f9c32ce6044159c1948c823fc3
danielgsantos15 <danir877@gmail.com>
2022-05-18 19:42:07 -0300

    feat: first features

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("teste")
+const wordsBase = ["teste" , "pasta" , "sexta" , "viola" , "pasto" , "tripa"]
+const secretWord = wordsBase[Math.floor(Math.random() * wordsBase.length)]
+
+console.log("palavra secreta:" , secretWord)
+
+let tentativas = () => {
+  let tentativa = wordsBase[Math.floor(Math.random() * wordsBase.length)]
+  console.log(tentativa)
+  return tentativa
+}
+
+let venceu = () => {
+  console.log("Você acertou!")
+}
+
+
+for (let index = 5; index > 0; index--) {
+  if (tentativas() == secretWord) {
+    venceu()
+    break
+  } else console.log(`Você errou, restam ${index} tentativas`)
+}
+
