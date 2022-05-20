9d540677563eaa1fe0864f9beafc98adf2056f23
danielgsantos15 <danir877@gmail.com>
2022-05-20 17:20:10 -0300

    feat: arrays compare created

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const wordBase = ["teste" , "pasta" , "sexta" , "viola" , "pasto" , "tripa", "noite" , "velho" , "placa" , "mosto" , "terra" , "garra" , "marra"]
 const secretWord = wordBase[Math.floor(Math.random() * wordBase.length)]
+const secretArray = Array.from(secretWord)
 
 // secret word generated automatcally
 console.log("palavra secreta:" , secretWord)
 
-// victory message
-let won = (attempt) => {
-  console.log(`parabéns! você acertou! ${attempt}`)
-}
+
 
 // compare with secret word and show informations of written word
 let tryes = (attempt) => {
   console.log(`${attempt} errou, tente novamente`)
+  let arrWord = Array.from(attempt)
+  return arraysCheck(arrWord)
+}
+
+// compare letter for letter of arrays
+let arraysCheck = (arrWord) => {
+  let arrTryed = arrWord
+  secretArray[0] == arrTryed[0]
+  secretArray[1] == arrTryed[1]
+  secretArray[2] == arrTryed[2]
+  secretArray[3] == arrTryed[3]
+  secretArray[4] == arrTryed[4]
 }
 
 // check written word in database
@@ -19,8 +29,13 @@ let tryes = (attempt) => {
 //   return wordBase.includes(attempt)
 // }
 
-let wordCompare = (attempt) => {
+let wordTrue = (attempt) => {
  console.log(`A palavra ${attempt} não existe`)
+}
+
+// victory message
+let won = (attempt) => {
+  console.log(`parabéns! você acertou! ${attempt}`)
 }
 
 // receive written word for the player in the frontend
@@ -32,6 +47,6 @@ let writtenWord = () => {
       if (attempt == secretWord) {
         return won(attempt)
       } return tryes(attempt)
-    } wordCompare(attempt)
+    } wordTrue(attempt)
 } 
 writtenWord()
