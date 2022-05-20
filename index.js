5a180c4a6609aaf14b0e306105f20f49bc12ffcd
danielgsantos15 <danir877@gmail.com>
2022-05-20 18:44:52 -0300

    feat: letters check

--- index.js
+++ index.js
@@ -1,35 +1,67 @@
-const wordBase = ["teste" , "pasta" , "sexta" , "viola" , "pasto" , "tripa", "noite" , "velho" , "placa" , "mosto" , "terra" , "garra" , "marra"]
+const wordBase = ["teste" , "pasta" , "sexta" , "viola"]
 const secretWord = wordBase[Math.floor(Math.random() * wordBase.length)]
 const secretArray = Array.from(secretWord)
 
 // secret word generated automatcally
 console.log("palavra secreta:" , secretWord)
 
+// compare letter for letter of arrays
 
+// * @params
+
+let greenCheck = (attempt) => {
+  if (attempt != secretWord) {
+    tryes(attempt)
+  } else won(attempt)
+  
+  let arrTryed = Array.from(attempt)
+  if (secretArray[0] == arrTryed[0]) {
+    console.log(arrTryed[0])
+  } else if (secretArray.includes(arrTryed[0]) == true) {
+    console.log(arrTryed[0].toUpperCase())
+  }else console.log("#")
+
+  if (secretArray[1] == arrTryed[1]) {
+    console.log(arrTryed[1])
+  } else if (secretArray.includes(arrTryed[1]) == true) {
+    console.log(arrTryed[1].toUpperCase())
+  } else console.log("#")
+
+  if (secretArray[2] == arrTryed[2]) {
+    console.log(arrTryed[2])
+  } else if (secretArray.includes(arrTryed[2]) == true) {
+    console.log(arrTryed[2].toUpperCase())
+  }else console.log("#")
+
+  if (secretArray[3] == arrTryed[3]) {
+    console.log(arrTryed[3])
+  } else if (secretArray.includes(arrTryed[3]) == true) {
+    console.log(arrTryed[3].toUpperCase())
+  }else console.log("#")
+
+  if (secretArray[4] == arrTryed[4]) {
+    console.log(arrTryed[4])
+  } else if (secretArray.includes(arrTryed[4]) == true) {
+    console.log(arrTryed[4].toUpperCase())
+  }else console.log("#")
+}
 
 // compare with secret word and show informations of written word
 let tryes = (attempt) => {
   console.log(`${attempt} errou, tente novamente`)
-  let arrWord = Array.from(attempt)
-  return arraysCheck(arrWord)
-}
-
-// compare letter for letter of arrays
-let arraysCheck = (arrWord) => {
-  let arrTryed = arrWord
-  secretArray[0] == arrTryed[0]
-  secretArray[1] == arrTryed[1]
-  secretArray[2] == arrTryed[2]
-  secretArray[3] == arrTryed[3]
-  secretArray[4] == arrTryed[4]
 }
 
 // check written word in database
-// let dataCheck = (attempt) => {
-//   return wordBase.includes(attempt)
-// }
+let dataCheck = (attempt) => {
+  if (wordBase.includes(attempt) == true) {
+    if (attempt == secretWord) {
+      greenCheck(attempt)
+    } else return greenCheck(attempt)
+  } else wordNotFound(attempt)
+}
 
-let wordTrue = (attempt) => {
+// if does not found written word on database
+let wordNotFound = (attempt) => {
  console.log(`A palavra ${attempt} não existe`)
 }
 
@@ -42,11 +74,6 @@ let won = (attempt) => {
 let writtenWord = () => {
   // let attempt = "poker"
   let attempt = wordBase[Math.floor(Math.random() * wordBase.length)]
-  // check if written word exists in database
-  if (wordBase.includes(attempt) == true) {
-      if (attempt == secretWord) {
-        return won(attempt)
-      } return tryes(attempt)
-    } wordTrue(attempt)
+  dataCheck(attempt)
 } 
 writtenWord()
