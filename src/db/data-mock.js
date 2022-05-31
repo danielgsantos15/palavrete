4588dca82f30dae6198a22843b36ed74c6c56b1f
danielgsantos15 <danir877@gmail.com>
2022-05-30 22:47:53 -0300

    refactor: mock database

--- src/db/data-mock.js
+++ src/db/data-mock.js
@@ -1,14 +1,14 @@
 const uuid = require("uuid")
 // const wordBase = ["teste", "testa", "pasta", "sexta", "sabes", "viola", "vidas", "pasto"]
 const wordBase = new Map([
-  [uuid(), "teste"],
-  [uuid(), "testa"],
-  [uuid(), "pasta"],
-  [uuid(), "pasto"],
-  [uuid(), "sexta"],
-  [uuid(), "sabes"],
-  [uuid(), "viola"],
-  [uuid(), "vidas"]
+  [uuid.v4(), "teste"],
+  [uuid.v4(), "testa"],
+  [uuid.v4(), "pasta"],
+  [uuid.v4(), "pasto"],
+  [uuid.v4(), "sexta"],
+  [uuid.v4(), "sabes"],
+  [uuid.v4(), "viola"],
+  [uuid.v4(), "vidas"]
 ])
 
 module.exports = wordBase
