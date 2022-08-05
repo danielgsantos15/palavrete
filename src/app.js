ed2794b0ac4493c6bc7310d3fc086f3d29a92848
danielgsantos15 <danir877@gmail.com>
2022-08-05 18:31:25 -0300

    feat: rendering public

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import express from 'express'
 
 const app = express()
+app.use(express.static('public'));
 
-app.use(express.static(__dirname + '../public'));
-
-// app.get('/', (req, res) => {
-//   // res.send('teste')
-//   res.status(200).render('../public/index.ejs')
-// })
+app.get('/', (req, res) => {
+  
+})
 
 export default app
