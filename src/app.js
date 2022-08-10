0dc8ae6e96249a6263b1166b10fecb393bb53e0c
danielgsantos15 <danir877@gmail.com>
2022-08-10 20:11:59 -0300

    feat: getting client word from front end

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import express from 'express'
+import { validate } from 'uuid'
+import StartPalavrete from './startPalavrete.js'
+import LetterValidator from './validator/letter.validator.js'
 
 const app = express()
+const lettervalidator = new LetterValidator(5)
 app.use(express.static('public'));
 
-app.get('/word', (req, res) => {
-  console.log('banana')
-  res.send({'data': 'attempt'})
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.post('/word', (req, res, next) => {
+  const { word } = req.body
+  const start = new StartPalavrete(word)
+  let secretWord = start.getSecretWordFromDatabase()
+  if (!start.hasClientWordOnDatabase()) {
+    res.status(404)
+    next()
+  }
+  const validatedArray = lettervalidator.compareWordsToClassifyTheLetterPosition(word, secretWord)
+  res.send(validatedArray)
 })
 
 export default app
