2362724e25b1b425fc45f2cb4225a8457a1d1ee8
danielgsantos15 <danir877@gmail.com>
2022-08-11 16:34:19 -0300

    refactor: tools file created

---
+++ public/helperTools.js
@@ -0,0 +1,25 @@
+// Requests
+const doRequests = async (request) => {
+  try {
+    const data = {
+      word: request.data
+    }
+    const url = `http://localhost:3000${request.resource}`
+    const response = await fetch(url, {
+      method: request.method,
+      body: JSON.stringify(data),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    return response.json()  
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const changeTheSelectedRow = (tipoDeSeletor, elementDOMToWork, actualStatus, statusToChange) => {
+  let variavel = elementDOMToWork.querySelectorAll(tipoDeSeletor)
+  for (let index = 0; index < variavel.length; index++) {
+    variavel[index].classList.remove(actualStatus)
+    variavel[index].classList.add(statusToChange)
+  }
+}
