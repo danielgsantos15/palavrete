2b1cf090027d537caac36368c8300522812b9b7e
danielgsantos15 <danir877@gmail.com>
2022-08-12 17:38:24 -0300

    feat: testing mock word

--- src/db/data-mock.js
+++ src/db/data-mock.js
@@ -1,14 +1,14 @@
 import { v4 } from "uuid"
 // const wordBase = ["teste", "testa", "pasta", "sexta", "sabes", "viola", "vidas", "pasto"]
 const wordBase = new Map([
-  [v4(), "teste"],
-  [v4(), "testa"],
-  [v4(), "pasta"],
-  [v4(), "pasto"],
-  [v4(), "sexta"],
-  [v4(), "sabes"],
-  [v4(), "viola"],
-  [v4(), "vidas"]
+  [v4(), "TESTE"],
+  // [v4(), "testa"],
+  // [v4(), "pasta"],
+  // [v4(), "pasto"],
+  // [v4(), "sexta"],
+  // [v4(), "sabes"],
+  // [v4(), "viola"],
+  // [v4(), "VIDAS"]
 ])
 
 export default wordBase
