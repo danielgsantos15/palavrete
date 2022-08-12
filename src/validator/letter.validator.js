6b548f1509a60a30b55a68ad196dfbf3c3b9a6ff
danielgsantos15 <danir877@gmail.com>
2022-08-12 17:38:00 -0300

    feat: testing mock word

--- src/validator/letter.validator.js
+++ src/validator/letter.validator.js
@@ -3,16 +3,17 @@ class LetterValidator {
     this.columns = columns
   }
   compareWordsToClassifyTheLetterPosition(clientArrayWord, secretArray) {
+  console.log(clientArrayWord, secretArray)
     let validatedWord = []
     for (let index = 0; index < this.columns; index++) {
       if (secretArray[index] == clientArrayWord[index]) {
-        validatedWord.push({ letter: clientArrayWord[index], classList: 'right' })
+        validatedWord.push({ letter: clientArrayWord[index], classList: 'letter-green' })
         continue
       } else if (secretArray.includes(clientArrayWord[index])) {
-        validatedWord.push({ letter: clientArrayWord[index], classList: 'displaced' })
+        validatedWord.push({ letter: clientArrayWord[index], classList: 'letter-yellow' })
         continue
       }
-      validatedWord.push({ letter: clientArrayWord[index], classList: 'wrong' })
+      validatedWord.push({ letter: clientArrayWord[index], classList: 'letter-red' })
     }
     return validatedWord
   }
