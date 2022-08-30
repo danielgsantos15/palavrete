353f6fd072538bfd18a30c6ffecbb810a3b3ccf8
danielgsantos15 <danir877@gmail.com>
2022-08-30 17:40:17 -0300

    refactor: some words changed in data-mock

--- src/db/data-mock.js
+++ src/db/data-mock.js
@@ -2,13 +2,13 @@ import { v4 } from "uuid"
 // const wordBase = ["teste", "testa", "pasta", "sexta", "sabes", "viola", "vidas", "pasto"]
 const wordBase = new Map([
   [v4(), "TRECO"],
-  [v4(), "TESTA"],
+  [v4(), "LENTE"],
   [v4(), "PASTA"],
-  [v4(), "PASTO"],
+  [v4(), "FESTA"],
   [v4(), "SEXTA"],
   [v4(), "SABES"],
   [v4(), "VIOLA"],
-  [v4(), "VIDAS"]
+  [v4(), "LETRA"]
 ])
 
 export default wordBase
