200af217660ccedb5652b0fbaee2c7e35004e6d0
danielgsantos15 <danir877@gmail.com>
2022-08-31 11:48:52 -0300

    feat: adding more test words to wordBase

--- src/db/data-mock.js
+++ src/db/data-mock.js
@@ -1,13 +1,24 @@
 import { v4 } from "uuid"
 // const wordBase = ["teste", "testa", "pasta", "sexta", "sabes", "viola", "vidas", "pasto"]
 const wordBase = new Map([
+  [v4(), "PERTO"],
+  [v4(), "TERCO"],
+  [v4(), "PRATO"],
+  [v4(), "GRITO"],
   [v4(), "TRECO"],
-  [v4(), "LENTE"],
-  [v4(), "PASTA"],
-  [v4(), "FESTA"],
-  [v4(), "SEXTA"],
-  [v4(), "SABES"],
-  [v4(), "VIOLA"],
+  [v4(), "TREVO"],
+  [v4(), "CESTO"],
+  [v4(), "CERVO"],
+  [v4(), "CERTO"],
+  [v4(), "PRETO"],
+  [v4(), "TERMO"],
+  [v4(), "SENSO"],
+  [v4(), "TREME"],
+  [v4(), "SERVE"],
+  [v4(), "RESTO"],
+  [v4(), "TERNO"],
+  [v4(), "TRENA"],
+  [v4(), "CESTA"],
   [v4(), "LETRA"]
 ])
 
