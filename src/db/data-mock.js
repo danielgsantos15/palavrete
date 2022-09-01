3b1dbce3e5d696f7605165190fa0c8b5188dcc9b
danielgsantos15 <danir877@gmail.com>
2022-09-01 10:41:51 -0300

    feat: getting word from word base

--- src/db/data-mock.js
+++ src/db/data-mock.js
@@ -1,25 +1,16 @@
-import { v4 } from "uuid"
-// const wordBase = ["teste", "testa", "pasta", "sexta", "sabes", "viola", "vidas", "pasto"]
-const wordBase = new Map([
-  [v4(), "PERTO"],
-  [v4(), "TERCO"],
-  [v4(), "PRATO"],
-  [v4(), "GRITO"],
-  [v4(), "TRECO"],
-  [v4(), "TREVO"],
-  [v4(), "CESTO"],
-  [v4(), "CERVO"],
-  [v4(), "CERTO"],
-  [v4(), "PRETO"],
-  [v4(), "TERMO"],
-  [v4(), "SENSO"],
-  [v4(), "TREME"],
-  [v4(), "SERVE"],
-  [v4(), "RESTO"],
-  [v4(), "TERNO"],
-  [v4(), "TRENA"],
-  [v4(), "CESTA"],
-  [v4(), "LETRA"]
-])
+import fs from 'fs'
+
+let wordBase = []
+
+fs.readFile('./src/db/words.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err)
+  }
+  let words = data.split('\r\n')
+  for (let word in words) {
+    let row = words[word]
+    wordBase.push(row)
+    }
+})
 
 export default wordBase
