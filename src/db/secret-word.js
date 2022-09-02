7b9646645907433335a35e56e8c4884718c56c3e
danielgsantos15 <danir877@gmail.com>
2022-09-02 17:02:23 -0300

    feat: put a word of wordBase in secretWord array

--- src/db/secret-word.js
+++ src/db/secret-word.js
@@ -1,20 +1,28 @@
 import fs from 'fs'
 
-class GenerateWordManager {
-  generateNewWord() { 
-    fs.readFile('./src/db/words.txt', 'utf-8', (err, data) => {
+let secretWord = []
+
+const randomNewWord = () => {
+  fs.readFile('./src/db/words.txt', 'utf-8', (err, data) => {
     if (err) {
       console.error(err)
     }
-    let secretWord = []
     let words = data.split('\r\n')
     let base = words[Math.floor(Math.random() * words.length)]
-    secretWord.push(base)
-    console.log(secretWord, base)
 
-    return base
+    for (let letter in base) {
+      secretWord.push(base[letter])
+    }
+
+    clearSecretWordArray()
+    return secretWord
   })
-  }
 }
 
-export default GenerateWordManager
+randomNewWord()
+
+const clearSecretWordArray = () => {
+  secretWord = []
+}
+
+export default secretWord
