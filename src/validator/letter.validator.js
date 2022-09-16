bbb36822db7b71e5f6bcd0c64024c2c87dd34581
danielgsantos15 <danir877@gmail.com>
2022-09-16 17:36:09 -0300

    feat: changing letter colors logic

--- src/validator/letter.validator.js
+++ src/validator/letter.validator.js
@@ -4,14 +4,17 @@ class LetterValidator {
   }
   compareWordsToClassifyTheLetterPosition(clientArrayWord, secretArray) {
     let validatedWord = []
+    let arrayToCheck = secretArray
+    // usar este array para excluir as letras verdes encontradas sem alterar o secretArray
     console.log(secretArray)
 
     for (let index = 0; index < this.columns; index++) {
+      pushRedLetter(validatedWord, clientArrayWord, index)
+
       if (secretArray.includes(clientArrayWord[index])) {
         pushGreenLetter(secretArray, validatedWord, clientArrayWord, index)
         continue
       }
-      pushRedLetter(validatedWord, clientArrayWord, index)
     }
     for (let index = 0; index < this.columns; index++) {
       pushYellowLetter(secretArray, validatedWord, clientArrayWord, index)
@@ -20,21 +23,35 @@ class LetterValidator {
   }
 }
 
-const pushGreenLetter = (secretArray, validatedWord, clientArrayWord, index) => {
-  let greenIndex = secretArray.indexOf(clientArrayWord[index])
-  secretArray[greenIndex] = '#'
-  validatedWord.push({ letter: clientArrayWord[index], classList: 'letter-green' })
-}
-
 const pushRedLetter = (validatedWord, clientArrayWord, index) => {
   validatedWord.push({ letter: clientArrayWord[index], classList: 'letter-red' })
 }
 
+const pushGreenLetter = (secretArray, validatedWord, clientArrayWord, index) => {
+  // validatedWord.push({ letter: clientArrayWord[index], classList: 'letter-green' })
+  validatedWord[index].classList = 'letter-green'
+  // let yellowIndex = secretArray.indexOf(clientArrayWord[index])
+  //   secretArray[yellowIndex] = '#'
+}
+
 const pushYellowLetter = (secretArray, validatedWord, clientArrayWord, index) => {
-  if (validatedWord[index].classList == 'letter-green' && clientArrayWord.indexOf(clientArrayWord[index]) != secretArray.indexOf(clientArrayWord[index])) {
-    console.log(clientArrayWord.indexOf(clientArrayWord[index]), clientArrayWord[index])
-    validatedWord[index].classList = 'letter-yellow'
+  if (validatedWord[index].classList != 'letter-green') {
+    return
   }
+  else if (clientArrayWord.indexOf(clientArrayWord[index]) == secretArray.indexOf(clientArrayWord[index])) {
+    return
+  }
+  let quantidade = secretArray.filter(x => x === clientArrayWord[index]).length
+  if (quantidade >= 1) {
+    console.log(validatedWord[index], secretArray.filter(x => x === clientArrayWord[index]).length)
+    validatedWord[index].classList = 'letter-yellow'
+    quantidade = -1
+  }
+    // let yellowIndex = secretArray.indexOf(clientArrayWord[index])
+    // secretArray[yellowIndex] = '*'
+    console.log(clientArrayWord.indexOf(clientArrayWord[index]), clientArrayWord[index])
+    console.log(secretArray.indexOf(clientArrayWord[index]), secretArray[index])
+    validatedWord[index].classList = 'letter-yellow'
 }
 
   // let yellowIndex = secretArray.indexOf(clientArrayWord[index])
