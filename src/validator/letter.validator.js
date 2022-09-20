160e7e791acb9c1923349a074c294bb26312ad56
danielgsantos15 <danir877@gmail.com>
2022-09-20 19:03:41 -0300

    feat: creating object to compare with wrote word

--- src/validator/letter.validator.js
+++ src/validator/letter.validator.js
@@ -4,64 +4,64 @@ class LetterValidator {
   }
   compareWordsToClassifyTheLetterPosition(clientArrayWord, secretArray) {
     let validatedWord = []
-    let arrayToCheck = secretArray
-    // usar este array para excluir as letras verdes encontradas sem alterar o secretArray
+    // usar este arrayToCompare para criar objetos e comparar com validatedWord
+    let arrayToCompare = []
+
     console.log(secretArray)
 
+    // loop para adicionar objetos aos arrays
     for (let index = 0; index < this.columns; index++) {
       pushRedLetter(validatedWord, clientArrayWord, index)
+      visibleLettersToCompare(arrayToCompare, secretArray, index)
 
+      // checa se cada letra em clientArrayWord exitem dentro do secretArray
       if (secretArray.includes(clientArrayWord[index])) {
-        pushGreenLetter(secretArray, validatedWord, clientArrayWord, index)
+        pushGreenLetter(arrayToCompare, secretArray, validatedWord, clientArrayWord, index)
+        notVisibleLettersToCompare(arrayToCompare, secretArray, index)
         continue
       }
     }
+
+    // loop que altera o classList do secretArray para 'letter-yellow'
     for (let index = 0; index < this.columns; index++) {
-      pushYellowLetter(secretArray, validatedWord, clientArrayWord, index)
+      pushYellowLetter(arrayToCompare, secretArray, validatedWord, clientArrayWord, index)
     }
+    console.log(arrayToCompare)
+    console.log(validatedWord)
     return validatedWord
   }
 }
 
+// adiciona secretLetter e status 'visible' para cada letra de arrayToCompare
+const visibleLettersToCompare = (arrayToCompare, secretArray, index) => {
+  arrayToCompare.push({ secretLetter: secretArray[index], status: 'visible' })
+}
+
+// torna invisivel as letras que foram encontradas no secretArray para serem desconsideradas na validação das próximas
+const notVisibleLettersToCompare = (arrayToCompare, secretArray, index) => {
+  arrayToCompare[index].status = 'not-visible'
+}
+
+// adiciona letters e classList: 'letter-red' para cada letra de validatedWord
 const pushRedLetter = (validatedWord, clientArrayWord, index) => {
   validatedWord.push({ letter: clientArrayWord[index], classList: 'letter-red' })
 }
 
-const pushGreenLetter = (secretArray, validatedWord, clientArrayWord, index) => {
-  // validatedWord.push({ letter: clientArrayWord[index], classList: 'letter-green' })
+// adiciona 'letter-green' nas letras que existem dentro do secretArray
+const pushGreenLetter = (arrayToCompare, secretArray, validatedWord, clientArrayWord, index) => {
   validatedWord[index].classList = 'letter-green'
-  // let yellowIndex = secretArray.indexOf(clientArrayWord[index])
-  //   secretArray[yellowIndex] = '#'
 }
 
-const pushYellowLetter = (secretArray, validatedWord, clientArrayWord, index) => {
+// Adiciona 'letter-yellow' nas letras que estão na posição incorreta
+const pushYellowLetter = (arrayToCompare, secretArray, validatedWord, clientArrayWord, index) => {
   if (validatedWord[index].classList != 'letter-green') {
     return
   }
   else if (clientArrayWord.indexOf(clientArrayWord[index]) == secretArray.indexOf(clientArrayWord[index])) {
     return
   }
-  let quantidade = secretArray.filter(x => x === clientArrayWord[index]).length
-  if (quantidade >= 1) {
-    console.log(validatedWord[index], secretArray.filter(x => x === clientArrayWord[index]).length)
-    validatedWord[index].classList = 'letter-yellow'
-    quantidade = -1
-  }
-    // let yellowIndex = secretArray.indexOf(clientArrayWord[index])
-    // secretArray[yellowIndex] = '*'
-    console.log(clientArrayWord.indexOf(clientArrayWord[index]), clientArrayWord[index])
-    console.log(secretArray.indexOf(clientArrayWord[index]), secretArray[index])
-    validatedWord[index].classList = 'letter-yellow'
+  validatedWord[index].classList = 'letter-yellow'
+  arrayToCompare[index].status = 'not-visible'
 }
-
-  // let yellowIndex = secretArray.indexOf(clientArrayWord[index])
-  // secretArray[yellowIndex] = '*'
-  // validatedWord.push({ letter: clientArrayWord[index], classList: 'letter-yellow' })
-  // console.log('yellow')
-
-// else if (secretArray[index] == clientArrayWord[index]) {
-//   pushYellowLetter(secretArray, validatedWord, clientArrayWord, index)
-//   continue
-// }
 
 export default LetterValidator
