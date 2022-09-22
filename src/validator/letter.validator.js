66fc02e6238415f73ce0d53a00386ee1648f3a42
danielgsantos15 <danir877@gmail.com>
2022-09-22 17:58:01 -0300

    feat: letters of guesses validation

--- src/validator/letter.validator.js
+++ src/validator/letter.validator.js
@@ -3,78 +3,79 @@ class LetterValidator {
     this.columns = columns
   }
   compareWordsToClassifyTheLetterPosition(clientArrayWord, secretArray) {
+    // usar secretArrayToComparation para criar objetos e comparar com validatedWord
+    let secretArrayToComparation = []
     let validatedWord = []
-    // usar este arrayToCompare para criar objetos e comparar com validatedWord
-    let arrayToCompare = []
 
+    clientArrayToCompare(validatedWord, clientArrayWord, this.columns)
+    visibleLettersToCompare(secretArrayToComparation, secretArray, this.columns)
+    notVisibleLettersToCompare(validatedWord, secretArrayToComparation, secretArray, clientArrayWord, this.columns)
+    
     console.log(secretArray)
 
-    pushRedLetter(validatedWord, clientArrayWord, this.columns)
-    visibleLettersToCompare(arrayToCompare, secretArray, this.columns)
-    
-    notVisibleLettersToCompare(validatedWord, arrayToCompare, secretArray, clientArrayWord, this.columns)
-    // loop para adicionar class e classList aos objetos dos arrays
-    for (let index = 0; index < this.columns; index++) {
-      // checa se cada letra em clientArrayWord exitem dentro do secretArray
-      if (secretArray.includes(clientArrayWord[index])) {
-        pushGreenLetter(arrayToCompare, secretArray, validatedWord, clientArrayWord, index)
-        continue
-      }
-    }
-
-    // loop que altera o classList do secretArray para 'letter-yellow'
-    for (let index = 0; index < this.columns; index++) {
-      pushYellowLetter(arrayToCompare, secretArray, validatedWord, clientArrayWord, index)
-    }
-    console.log(arrayToCompare)
-    console.log(validatedWord)
     return validatedWord
   }
 }
 
 // cria objeto que adiciona letters e classList: 'letter-red' em todas as letras de validatedWord
-const pushRedLetter = (validatedWord, clientArrayWord, columns) => {
+const clientArrayToCompare = (validatedWord, clientArrayWord, columns) => {
   for (let index = 0; index < columns; index++) {
     validatedWord.push({ letter: clientArrayWord[index], classList: 'letter-red' })
   }
 }
 
-// cria objeto que adiciona secretLetter e status 'visible' em todas as letras de arrayToCompare
-const visibleLettersToCompare = (arrayToCompare, secretArray, columns) => {
+// cria objeto que adiciona secretLetter e status 'visible' em todas as letras de secretArrayToComparation
+const visibleLettersToCompare = (secretArrayToComparation, secretArray, columns) => {
   for (let index = 0; index < columns; index++) {
-    arrayToCompare.push({ secretLetter: secretArray[index], status: 'visible' })
+    secretArrayToComparation.push({ secretLetter: secretArray[index], status: 'visible' })
   }
 }
 
 // torna invisivel as letras que foram encontradas no secretArray para serem desconsideradas na validação das próximas
-const notVisibleLettersToCompare = (validatedWord, arrayToCompare, secretArray, clientArrayWord, columns) => {
+const notVisibleLettersToCompare = (validatedWord, secretArrayToComparation, secretArray, clientArrayWord, columns) => {
   for (let index = 0; index < columns; index++) {
     // checa se cada letra em clientArrayWord exitem dentro do secretArray
     if (secretArray.includes(clientArrayWord[index])) {
       let indexLetter = validatedWord[index].letter
-      let objectIndex = arrayToCompare.findIndex(i => i.secretLetter === indexLetter && i.status === 'visible')
-      if (arrayToCompare[objectIndex] === undefined ) {
+      let objectIndex = secretArrayToComparation.findIndex(i => i.secretLetter === indexLetter && i.status === 'visible' || i.secretLetter === indexLetter && i.status === 'not-visible')
+      if (secretArrayToComparation[objectIndex] === 'not-visible' ) {
+        pushLetterColors(secretArrayToComparation, validatedWord, index, objectIndex)
         return
       }
-      arrayToCompare[objectIndex].status = 'not-visible'
+      pushLetterColors(secretArrayToComparation, validatedWord, index, objectIndex)
+      secretArrayToComparation[objectIndex].status = 'not-visible'
     }
   }
 }
 
+// Função que adiciona cores a partir do status visible ou not-visible
+const pushLetterColors = (secretArrayToComparation, validatedWord, index, objectIndex) => {
+  if (validatedWord[index].letter == secretArrayToComparation[index].secretLetter) {
+    pushGreenLetter(validatedWord, index)
+    return  
+  } 
+  else if (secretArrayToComparation[objectIndex].status == 'visible') {
+    pushYellowLetter(validatedWord, index)
+  }
+}
+
 // adiciona 'letter-green' nas letras que existem dentro do secretArray
-const pushGreenLetter = (arrayToCompare, secretArray, validatedWord, clientArrayWord, index) => {
+const pushGreenLetter = (validatedWord, index) => {
   validatedWord[index].classList = 'letter-green'
+  yellowCheck(validatedWord, index)
 }
 
 // adiciona 'letter-yellow' nas letras que estão na posição incorreta
-const pushYellowLetter = (arrayToCompare, secretArray, validatedWord, clientArrayWord, index) => {
-  if (validatedWord[index].classList != 'letter-green') {
-    return
-  }
-  else if (clientArrayWord.indexOf(clientArrayWord[index]) == secretArray.indexOf(clientArrayWord[index])) {
-    return
-  }
+const pushYellowLetter = (validatedWord, index) => {
   validatedWord[index].classList = 'letter-yellow'
+}
+
+const yellowCheck = (validatedWord, index) => {
+  for (let i = 0; i < validatedWord.length; i++) {
+    if (validatedWord[i].letter == validatedWord[index].letter && validatedWord[i].classList == 'letter-yellow') {
+      validatedWord[i].classList = 'letter-red'
+    }
+  }
 }
 
 export default LetterValidator
